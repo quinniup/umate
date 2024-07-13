4de4628c3b2f311bc5f197932994e0483ff16ce7
G.T <howzequinn@gmail.com>
2024-07-14 03:07:10 +0800

    feat: init project. [#]

---
+++ rust-lib/dart-ffi/src/example.rs
@@ -0,0 +1,5 @@
+
+#[no_mangle]
+pub extern fn sum(n1: i32, n2: i32) -> i32 {
+    n1 + n2 + 1
+}
---
+++ umate-flutter/android/app/src/main/kotlin/com/example/umate/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.umate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
